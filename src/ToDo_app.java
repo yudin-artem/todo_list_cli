c4ed47bed0fb65ac70e660a704d05b191e81e653
yudin-artem <artemyudin.2006@yandex.ru>
2025-09-26 13:59:17 +0300

    base class

---
+++ src/ToDo_app.java
@@ -0,0 +1,40 @@
+
+
+public class ToDo_app {
+
+
+    public String create_note() {
+
+        return "";
+    }
+
+    public String edit_note() {
+
+        return "";
+    }
+
+    public String delete_note() {
+
+        return "";
+    }
+
+    public String set_deadline() {
+
+        return "";
+    }
+
+    public String pin_note() {
+
+        return "";
+    }
+
+    public String sort_ByDate() {
+
+        return "";
+    }
+
+    public String search_ByAttribute() {
+
+        return "";
+    }
+}
